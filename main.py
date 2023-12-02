14dc2ef848d55493c68f890eae1a0c474671f111
Qusai GAZAWY <candotube@gmail.com>
2023-12-02 13:14:39 +0300

    Made some changes

--- main.py
+++ main.py
@@ -2,22 +2,22 @@
 from flask import Flask, render_template,request, redirect
 from datetime import datetime
 
-year = (datetime.now().year) - 2010 #nice
+
 
 app = Flask(__name__)
 
 # İçerik sayfasını çalıştırma
 @app.route('/')
 def index():
-    return render_template('index.html')
+    year = (datetime.now().year) - 1998 #nice
+    return render_template('index.html', year = year)
 
 
 # Dinamik beceriler
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python,
-                           year = year)
+    return render_template('index.html', button_python=button_python)
 
 
 if __name__ == "__main__":
